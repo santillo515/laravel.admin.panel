d596f30d6160e6f308b8c506303af79b7c65db10
santillo515 <santillo515@mail.ru>
2020-01-26 19:06:45 +0700

    Create AdminLTE, Bread Crumbs, Meta Tags

--- resources/views/blog/admin/main/index.blade.php
+++ resources/views/blog/admin/main/index.blade.php
@@ -1,20 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.app_admin')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard DISABLED</div>
 
-                    <div class="card-body">
-
-                        ADMIN   ADMIN
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="content-header">
+        @component('blog.admin.components.breadcrumb')
+            @slot('title') Панель управления @endslot
+            @slot('parent') Главная @endslot
+            @slot('active') @endslot
+        @endcomponent
     </div>
+
 @endsection
 
 
